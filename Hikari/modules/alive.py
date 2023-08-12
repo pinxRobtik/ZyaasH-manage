6db1f5c2ea32405717cf867d72d683e8e6cd7a46
pinxRobtik <129358594+pinxRobtik@users.noreply.github.com>
2023-08-13 01:35:02 +0700

    update alive.py

--- Hikari/modules/alive.py
+++ Hikari/modules/alive.py
@@ -9,16 +9,16 @@ from Hikari.events import register
 from Hikari import telethn as tbot
 
 
-PHOTO = "https://telegra.ph/file/1e0543558ca953ab9bb47.jpg"
+PHOTO = "https://telegra.ph//file/4b747318ec1a036433a02.jpg"
 
 @register(pattern=("/alive"))
 async def awake(event):
   TEXT = f"**ʜɪ [{event.sender.first_name}](tg://user?id={event.sender.id}), ᴀᴋᴜ Yᴀᴀsʜ Mᴜsɪᴄ.** \n\n"
   TEXT += "๏ **ᴀᴋᴜ sᴇʟᴀʟᴜ ʜɪᴅᴜᴘ ᴅᴀɴ ʙᴇᴋᴇʀᴊᴀ** \n\n"
-  TEXT += f"๏ **ᴍʏ ᴏᴡɴᴇʀ : [Dexz](https://t.me/Dexzishere)** \n\n"
+  TEXT += f"๏ **ᴍʏ ᴏᴡɴᴇʀ : [reza](https://t.me/inirezatentunya)** \n\n"
   TEXT += f"๏ **ʟɪʙʀᴀʀʏ ᴠᴇʀsɪᴏɴ :** `{telever}` \n\n"
   TEXT += f"๏ **ᴛᴇʟᴇᴛʜᴏɴ ᴠᴇʀsɪᴏɴ :** `{tlhver}` \n\n"
   TEXT += f"๏ **ᴘʏʀᴏɢʀᴀᴍ ᴠᴇʀsɪᴏɴ :** `{pyrover}` \n\n"
   TEXT += "**ᴛᴇʀɪᴍᴀᴋᴀsɪʜ sᴜᴅᴀʜ ᴍᴇɴᴀᴍʙᴀʜᴋᴀɴ ᴋᴜ ᴅɪsɪɴɪ 🔥**"
-  BUTTON = [[Button.url("ʜᴇʟᴘ​", "https://t.me/Dark_musikbot?start=help"), Button.url("ᴅᴏɴᴀsɪ ​", "https://t.me/Dexzishere")]]
+  BUTTON = [[Button.url("ʜᴇʟᴘ​", "https://t.me/rezaxmusicbot?start=help"), Button.url("ᴅᴏɴᴀsɪ ​", "https://t.me/inirezatentunya")]]
   await tbot.send_file(event.chat_id, PHOTO, caption=TEXT,  buttons=BUTTON)
